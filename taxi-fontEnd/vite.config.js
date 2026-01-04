2b1543de0a0001cf9391fa803ad491b82d3eadb7
ChickenTTO <datchutich69@gmail.com>
2026-01-04 17:52:21 +0700

    fix vite

--- taxi-fontEnd/vite.config.js
+++ taxi-fontEnd/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Thay đổi công cụ nén CSS để tránh lỗi lightningcss
-    cssMinify: 'esbuild', 
+    cssMinify: false, 
   },
 })
